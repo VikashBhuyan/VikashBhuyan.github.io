649cd41c1a147e34601e01dbc6ffb63f72e22009
VikashBhuyan <vikashbhuyan2000@gmail.com>
2023-06-21 13:31:44 +0530

    Added latest changes

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
-const resume_btn1 = document.getElementById("resume-button-1")
-const resume_btn2 = document.getElementById("resume-button-2")
-const resume_btn3 = document.getElementById("resume-button")
 
+
+
+const typed = new Typed('.multiple-text', {
+  strings: ['Full Stack Web Developer', 'Backend Developer', 'Frontend Developer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backdelay: 1000,
+  loop: true
+})
+
+const resume_btn1 = document.querySelector("#resume-button-1");
+const resume_btn2 = document.querySelector("#resume-button-2");
+const  classbntn = document.querySelector("#resume-button");
 resume_btn1.addEventListener("click", (e) => {
   e.preventDefault();
   window.open(
-   "https://drive.google.com/file/d/1e6m0svWwur-BFKexneTrXsZxZYgM6ix0/view?usp=sharing"
+    "https://drive.google.com/file/d/1kp6GP_eb9IjysV7WGj6ilHDnylv3TvGP/view"
   );
   window.location.href =
-    "https://drive.google.com/file/d/1e6m0svWwur-BFKexneTrXsZxZYgM6ix0/view?usp=sharing";
+    "https://drive.google.com/uc?export=download&id=1e6m0svWwur-BFKexneTrXsZxZYgM6ix0";
 });
 
 resume_btn2.addEventListener("click", (e) => {
   e.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/1e6m0svWwur-BFKexneTrXsZxZYgM6ix0/view?usp=sharing"
+    "https://drive.google.com/file/d/1kp6GP_eb9IjysV7WGj6ilHDnylv3TvGP/view"
   );
   window.location.href =
-    "https://drive.google.com/file/d/1e6m0svWwur-BFKexneTrXsZxZYgM6ix0/view?usp=sharing";
+    "https://drive.google.com/uc?export=download&id=1e6m0svWwur-BFKexneTrXsZxZYgM6ix0";
 });
-resume_btn3.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.open(
-      "https://drive.google.com/file/d/1e6m0svWwur-BFKexneTrXsZxZYgM6ix0/view?usp=sharing"
-    );
-    window.location.href =
-      "https://drive.google.com/file/d/1e6m0svWwur-BFKexneTrXsZxZYgM6ix0/view?usp=sharing";
-  });
-  document.addEventListener('DOMContentLoaded', function() {
-    const resume_btn2 = document.querySelector("#resume-button-1");
-    resume_btn2.addEventListener("click", (e) => {
-      e.preventDefault();
-      window.location.href = "resume/Vikash_Bhuyan_Resume.pdf";
-    });
-  });
-document.addEventListener('DOMContentLoaded', function() {
-    const resume_btn2 = document.querySelector("#resume-button-2");
-    resume_btn2.addEventListener("click", (e) => {
-      e.preventDefault();
-      window.location.href = "resume/Vikash_Bhuyan_Resume.pdf";
-    });
-  });
+
+classbntn.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.open(
+    "https://drive.google.com/file/d/1kp6GP_eb9IjysV7WGj6ilHDnylv3TvGP/view"
+  );
+  window.location.href =
+    "https://drive.google.com/uc?export=download&id=1e6m0svWwur-BFKexneTrXsZxZYgM6ix0";
+});
